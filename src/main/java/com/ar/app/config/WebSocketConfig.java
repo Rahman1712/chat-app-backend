34468833d1e80f3a808dd920da89a698b72fe5e7
abdul rahman <abdulrahman1712@gmail.com>
2024-03-28 14:32:19 +0530

    updation in WS Config , CORS link

--- src/main/java/com/ar/app/config/WebSocketConfig.java
+++ src/main/java/com/ar/app/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
-	@Value("${cors.set.allowed.origins}")
+	@Value("${cors.set.allowed.ws.origins}")
 	private String[] CROSS_ORIGIN_URLS;
 	
     @Override
